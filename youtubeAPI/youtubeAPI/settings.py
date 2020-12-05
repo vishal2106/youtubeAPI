a695dde29d9d06568143821c909c034ce8bc7aa9
vishal2106 <vishalkhandelwaal@gmail.com>
2020-12-05 19:13:33 +0530

    removed creds

--- youtubeAPI/youtubeAPI/settings.py
+++ youtubeAPI/youtubeAPI/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,7 +117,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-GOOGLE_API_KEY = ''
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
